28152d94abb68a887437788e4ae9f85d601ba85f
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-06-20 14:32:37 +0200

    Update parseSortParams.js

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,6 +1,6 @@
 import { SORT_ORDER } from "../index.js"
 
-const parseSortOder = (sortOrder) => {
+const parseSortOrder = (sortOrder) => {
     const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
     if (isKnownOrder) return sortOrder;
     return SORT_ORDER.ASC;
