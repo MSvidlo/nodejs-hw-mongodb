2371a43ac4e971227e6850a7d8abc7ac2b529c2c
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-06-20 15:55:32 +0200

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { createContactController, deleteContactController, getContactByIdController, getContactController, patchContactController } from '../controllers/contacts.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema } from '../validation/contatcs.js';
+import { createContactShema } from '../validation/contatcs.js';
 
 
 const router = Router();
@@ -13,11 +13,11 @@ router.get('/', ctrlWrapper(getContactController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
 router.post('/',
-    validateBody(createContactSchema),
+    validateBody(createContactShema),
     ctrlWrapper(createContactController));
 
 router.patch('/:contactId',
-    validateBody(createContactSchema),
+    validateBody(createContactShema),
     ctrlWrapper(patchContactController));
 
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
